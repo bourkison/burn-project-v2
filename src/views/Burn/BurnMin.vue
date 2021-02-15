f5ba86d8cde4f0b91477c86a70d6c80b869a5818
bourkison <harrisonbourke97@gmail.com>
2021-02-15 22:12:55 +1100

    Added Burn Min

--- src/views/Burn/BurnMin.vue
+++ src/views/Burn/BurnMin.vue
@@ -2,13 +2,27 @@
     <v-card outlined class="burnMin">
         <v-container>
             <v-row justify="center" align="center">
-                <v-col cols="12" md="9">
+                <v-col cols="12" md="8">
                     <h2>{{ recentWorkout.name }}</h2>
                 </v-col>
-                <v-col cols="12" md="3" style="text-align: right;">
-                    <em>{{ recentWorkout.createdAtText }}</em>
+                <v-col cols="12" md="4" style="text-align: right;">
+                    <em>{{ timeString}} | {{ recentWorkout.createdAtText }}</em>
                 </v-col>
             </v-row>
+            <v-expansion-panels>
+                <v-expansion-panel v-for="exercise in recentWorkout.exercises" :key="exercise.id">
+                    <v-expansion-panel-header>{{ exercise.name }}</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <v-sheet align="center">
+                            <v-row align="center" justify="center" v-for="(set, index) in exercise.sets" :key="index">
+                                <v-col cols="12" sm="3">
+                                    {{ set.measureAmount }}
+                                </v-col>
+                            </v-row>
+                        </v-sheet>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-container>
     </v-card>
 </template>
@@ -23,8 +37,26 @@ export default {
         }
     },
 
-    created: function() {
-        console.log(this.$props.recentWorkout);
+    data() {
+        return {
+            timeString: ''
+        }
+    },
+
+    mounted: function() {
+        if (this.$props.recentWorkout.duration) {
+            let hours = Math.floor((this.$props.recentWorkout.duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 24));
+            let minutes = (Math.floor((this.$props.recentWorkout.duration % (1000 * 60 * 60)) / (1000 * 60))).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            let seconds = (Math.floor((this.$props.recentWorkout.duration % (1000 * 60)) / 1000)).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+
+            if (!hours) {
+                this.timeString = minutes + ":" + seconds;
+            } else {
+                this.timeString = hours + ":" + minutes + ":" + seconds;
+            }
+        } else {
+            this.timeString = "00:00"
+        }
     }
 }
 </script>
