ca1abaa981f47c3bac092cb260073b423231ea3b
bourkison <harrisonbourke97@gmail.com>
2021-02-15 21:40:43 +1100

    Pulling in recent burns

---
+++ src/views/Burn/BurnMin.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card outlined class="burnMin">
+        <v-container>
+            <v-row justify="center" align="center">
+                <v-col cols="12" md="9">
+                    <h2>{{ recentWorkout.name }}</h2>
+                </v-col>
+                <v-col cols="12" md="3" style="text-align: right;">
+                    <em>{{ recentWorkout.createdAtText }}</em>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'BurnMin',
+    props: {
+        recentWorkout: {
+            type: Object,
+            required: true
+        }
+    },
+
+    created: function() {
+        console.log(this.$props.recentWorkout);
+    }
+}
+</script>
+
+<style scoped>
+    .burnMin {
+        margin-bottom: 15px;
+    }
+</style>
--- src/views/Burn/RecentBurn.vue
+++ src/views/Burn/RecentBurn.vue
@@ -1,9 +1,45 @@
 <template>
-    <div>Recent</div>
+    <v-sheet rounded="lg">
+        <v-container v-if="!isLoading">
+            <BurnMin v-for="recentWorkout in recentWorkouts" :recentWorkout="recentWorkout" :key="recentWorkout.rId"></BurnMin>
+        </v-container>
+        <v-container v-else>
+            <div align="center"><v-progress-circular indeterminate centered></v-progress-circular></div>
+        </v-container>
+    </v-sheet>
 </template>
 
 <script>
+import { db } from '../../firebase'
+
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+import BurnMin from './BurnMin.vue'
+
 export default {
-    name: 'RecentBurn'
+    name: 'RecentBurn',
+    components: { BurnMin },
+    data() {
+        return {
+            isLoading: true,
+            recentWorkouts: []
+        }
+    },
+
+    created: function() {
+        dayjs.extend(relativeTime);
+
+        db.collection("users").doc(this.$store.state.userProfile.data.uid).collection("recentWorkouts").orderBy("createdAt", "desc").get().then(recentWorkoutsSnapshot => {
+            recentWorkoutsSnapshot.forEach(recentWorkout => {
+                let d = recentWorkout.data();
+                d.rId = recentWorkout.id;
+                d.createdAtText = dayjs(dayjs.unix(d.createdAt.seconds)).fromNow();
+                this.recentWorkouts.push(d);
+            })
+
+            this.isLoading = false;
+        })
+    }
 }
 </script>
