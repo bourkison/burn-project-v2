46b81d93bf3655b8c745fa6ce69b33912d7afa4e
bourkison <harrisonbourke97@gmail.com>
2021-02-19 00:44:29 +1100

    Home page now shows Feed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,30 @@
 <template>
-    <v-container>
+    <v-container fluid>
         <v-row>
-        <v-spacer></v-spacer>
-        <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-            <!--  -->
-            </v-sheet>
-        </v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="12" sm="3" xl="2">
+                <v-sheet rounded="lg" min-height="268">
+                <!--  -->
+                </v-sheet>
+            </v-col>
 
-        <v-col cols="12" sm="6">
-            <v-sheet min-height="70vh" rounded="lg">
-            <v-container v-if="$store.state.userProfile.loggedIn">
-                <div v-if="!isLoading">
-                    Welcome {{ $store.state.userProfile.docData.username }}
-                </div>
-                <div v-else>
-                    <div align="center"><v-progress-circular indeterminate centered></v-progress-circular></div>
-                </div>
-            </v-container>
-            <v-container v-else>
-                <router-link to="/login">Login</router-link> or <router-link to="/signup">Sign Up</router-link> to get started.
-            </v-container>
-            </v-sheet>
-        </v-col>
+            <v-col cols="12" sm="6" lg="5" xl="4">
+                <v-sheet min-height="70vh" rounded="lg">
+                    <v-container v-if="$store.state.userProfile.loggedIn">
+                        <Feed></Feed>
+                    </v-container>
+                    <v-container v-else>
+                        <router-link to="/login">Login</router-link> or <router-link to="/signup">Sign Up</router-link> to get started.
+                    </v-container>
+                </v-sheet>
+            </v-col>
 
-        <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-            <!--  -->
-            </v-sheet>
-        </v-col>
-        <v-spacer></v-spacer>
+            <v-col cols="12" sm="3" xl="2">
+                <v-sheet rounded="lg" min-height="268">
+                <!--  -->
+                </v-sheet>
+            </v-col>
+            <v-spacer></v-spacer>
         </v-row>
     </v-container>
 </template>
@@ -37,28 +32,16 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import { functions } from '@/firebase'
+import Feed from '@/views/Posts/Feed.vue'
 
 export default {
     name: 'Home',
-    components: { },
+    components: { Feed },
     data() {
         return {
-            isLoading: true
+            // isLoading: false
         }
     },
-
-    created: function() {
-        let buildFeedFunction = functions.httpsCallable("buildFeed");
-
-        buildFeedFunction().then(result => {
-            console.log("Success:", result);
-            this.isLoading = false;
-        }).catch(e => {
-            console.error("Failure", e);
-            this.isLoading = false;
-        })
-    }
 }
 </script>
 
---
+++ src/views/Posts/Feed.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="!isLoading">
+        <h1>Welcome, {{$store.state.userProfile.docData.username}}</h1>
+        <v-row v-for="post in posts" :key="post">
+            <v-col cols="12" sm="12">
+                <ViewPostMin :postId="post"></ViewPostMin>
+            </v-col>
+        </v-row>
+    </div>
+    <div v-else>
+        <div align="center"><v-progress-circular indeterminate centered></v-progress-circular></div>
+    </div>
+</template>
+
+<script>
+import { functions } from '@/firebase'
+import ViewPostMin from '@/views/Posts/ViewPostMin.vue'
+
+export default {
+    name: 'Feed',
+    components: { ViewPostMin },
+    data() {
+        return {
+            isLoading: true,
+            posts: [],
+        }
+    },
+
+    created: function() {
+        let buildFeedFunction = functions.httpsCallable("buildFeed");
+
+        buildFeedFunction().then(result => {
+            this.posts = result.data.posts;
+            this.isLoading = false;
+        }).catch(e => {
+            console.error("Failure", e);
+            this.isLoading = false;
+        })
+    }
+}
+</script>
